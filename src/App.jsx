48a5ebaea032d6d57f57e72bd2d38c882ea3e13a
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2024-02-12 11:53:38 +0100

    Fixing lazy load

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import "./App.css";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
-
 import { Suspense, lazy } from "react";
 
-const Home = lazy(() => {
-  import("./components/home/home");
-});
-const Movies = lazy(() => {
-  import("./components/movies/movies");
-});
-const MovieDetails = lazy(() => {
-  import("./components/moviedetails/moviedetails");
-});
+const Home = lazy(() => import("./components/home/home"));
+const Movies = lazy(() => import("./components/movies/movies"));
+const MovieDetails = lazy(() =>
+  import("./components/moviedetails/moviedetails")
+);
 
-const Cast = lazy(() => {
-  import("./components/cast/cast");
-});
-const Reviews = lazy(() => {
-  import("./components/review/reviews");
-});
+const Cast = lazy(() => import("./components/cast/cast"));
+const Reviews = lazy(() => import("./components/review/reviews"));
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/movies">Movies</NavLink>
+        <header className="header">
+          <nav className="nav">
+            <NavLink to="/" activeClassName="active">
+              Home
+            </NavLink>
+            <NavLink to="/movies" activeClassName="active">
+              Movies
+            </NavLink>
           </nav>
         </header>
         <Suspense fallback={<div>Loading... </div>}>
